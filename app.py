a88aa3cef2dc459ad5571c25efaefed4bebca40a
Nick-Cora <72200919+Nick-Cora@users.noreply.github.com>
2021-12-09 10:03:57 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -2,14 +2,17 @@ from flask import Flask, render_template, request
 from lib.config import *
 
 if not DUMMY:
-    from lib.alphabot import Alphabot
+    from lib.alphabot import AlphaBot
 else:
     from lib.alphabot_dummy import AlphaBot
 
 
 robot = AlphaBot()
 app = Flask(__name__)
-
+status = {'right'  :  0,
+              'left'   :  0,
+              'up'     :  0,
+              'down'   :  0}
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
@@ -18,11 +21,16 @@ def index():
                  'left'   :  robot.left,
                  'up'     :  robot.forward,
                  'down'   :  robot.backward}
+    
 
     if request.method == 'POST':
         for key, value in movements.items():
             if request.form.get(key) == key.upper():
-                print(value())
+                if status[key] == 1:
+                    robot.stop()
+                else:
+                    print(value())
+                status[key] = not status[key]
         
     elif request.method == 'GET':
         return render_template('index.html')
@@ -32,4 +40,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='localhost')
+    app.run(debug=True, host='192.168.0.134')
